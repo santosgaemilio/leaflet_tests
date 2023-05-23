54acd4d216d0ccc83730b4f04944fdff83aec510
emilio_santos <santosgaemilio@gmail.com>
2023-05-22 23:40:32 -0600

    pt 2

---
+++ test_1/map.js
@@ -0,0 +1,27 @@
+var map = L.map('map').setView([25.686768, -100.319040], 13);
+
+L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([25.686768, -100.319040],{color: "red"}).addTo(map);
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup(); 
+
+fetch('monterrey.geojson')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    var featureLayer = L.geoJSON(data.features, {
+      style: {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5
+      }
+    });
+    featureLayer.addTo(map);
+    map.fitBounds(featureLayer.getBounds());
+  })
+
+  
