deba38e1ad03e6f386022daac4e724eca8280d7c
emilio_santos <santosgaemilio@gmail.com>
2023-05-25 22:30:21 -0600

    new

--- map.js
+++ map.js
@@ -6,7 +6,7 @@ var map = L.map('map').setView([25.686768, -100.319040], 11);
 // }).addTo(map);
 
 var base = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
+    maxZoom: 25,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
@@ -49,11 +49,17 @@ fetch('monterrey.geojson')
 
  
 // TEST DE IMAGEN | IMAGEN MUY GRANDE
-var imageUrl = 'foto4.png',
+var imageUrl = 'foto5.png',
     imageBounds = [[25.696315,-100.273014], [25.692045,-100.266903]];
 L.imageOverlay(imageUrl, imageBounds).addTo(map);
 
-var img_new = L.marker([25.696315,-100.273014], [25.692045,-100.266903]).bindPopup("Citadel").addTo(map);
+var img_new = L.marker([25.696315,-100.273014], [25.692045,-100.266903]).bindPopup("ppp").addTo(map);
+
+var imageUrl2 = 'foto_Test2.png',
+    imageBounds2 = [[25.735443,-100.318744], [25.719559,-100.303911]];
+L.imageOverlay(imageUrl2, imageBounds2).addTo(map);
+
+var img_new2 = L.marker([25.736075,-100.320327], [25.718592,-100.303502]).bindPopup("ppp2").addTo(map);
 
 
   // var geojsonFilePath = './monterrey.geojson';
